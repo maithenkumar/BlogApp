cafe85b7fe68fe0637abfbb4d8d74f540f8e5ae2
maithenkumar <maitheen1905@gmail.com>
2023-09-10 23:18:51 +0530

    masmdkas

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 // import 'package:flutter_application_1/screens/form.dart';
 // import 'package:flutter_application_1/screens/login_page.dart';
 // import 'package:flutter_application_1/widgets/text_form.dart';
 // import 'package:flutter_application_1/screens/form.dart';
  import 'package:flutter_application_1/screens/profile_page.dart';
-=======
-
-import 'home_page.dart';
-import 'screens/BlogWritingPage.dart';
-import 'screens/HomeScreen.dart';
-import 'screens/blogDetails.dart';
->>>>>>> 85837a54c7dde409e9bd7f79015fcbbe05bdc450
 
 void main() {
   runApp(const MyApp());
@@ -24,14 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const MaterialApp(
-<<<<<<< HEAD
+
       debugShowCheckedModeBanner:false,
       title:'Blog Application',
       home:ProfilePage(),
-=======
-      debugShowCheckedModeBanner: false,
-      title:'My App',
-      home: BologDetails(),
+
+      
     );
   }
 }
